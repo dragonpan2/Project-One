543a46e128069e9a4bf4dc4b90917e1b27420a14
dragonpan2 <panbeining@hotmail.com>
2016-12-26 09:27:55 +0800

    Initial Commit

---
+++ Java-Project-One/src/java/project/one/JavaProjectOne.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java.project.one;
+
+/**
+ *
+ * @author Lin-Li
+ */
+public class JavaProjectOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
